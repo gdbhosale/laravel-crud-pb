3eaf08a1dfbaeb698bae6c22c95845c30aefac6e
gdbhosale <gdb.sci123@gmail.com>
2016-06-01 12:26:46 +0530

    phone added in user

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,6 +15,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
+		'phone' => "8888888888",
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
